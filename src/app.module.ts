5389a4ddc7cfb681a5b25679274a1fb087c7f25f
javier carrasco <javier@MacBook-Air-de-javier.local>
2023-09-02 17:17:01 -0400

    envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,20 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
     PokemonModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     CommonModule,
     SeedModule,
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB),
   ],
 })
 export class AppModule {}
